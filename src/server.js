157bdf410470492b69d5a0609a132f9f769df14e
AM738349 <96925591+Amirreza10101998@users.noreply.github.com>
2023-03-03 11:54:52 +0000

    multer added

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 import express from "express";
 import cors from "cors"
 import listEndpoints from "express-list-endpoints";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 
 /*-----Imported Routers-----*/
 import productsRouter from "./products/index.js"
+
 
 /*-----Imported Error Handlers-----*/
 
@@ -13,8 +16,20 @@ const port = 3001;
 server.use(cors());
 server.use(express.json());
 
+
+const getPublicFilePath = () => {
+  const _filename = fileURLToPath(import.meta.url);
+  const _dirname = dirname(_filename);
+  const publicFilePath = path.join(_dirname, "../public");
+
+  return publicFilePath;
+};
+
+server.use(express.static(getPublicFilePath()));
+
 /*-----Routers-----*/
 server.use("/products", productsRouter);
+
 
 console.table(listEndpoints(server));
 
